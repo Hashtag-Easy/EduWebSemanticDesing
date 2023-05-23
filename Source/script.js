ffc3f2b964286fd6bcc0542bcf510bd40c4d31b9
Kacper Żabiński <kacperzab0@wp.pl>
2023-05-23 19:20:30 +0200

    Stage 1

--- Source/script.js
+++ Source/script.js
@@ -1,4 +1,3 @@
-// Smooth scrolling on navigation links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
     e.preventDefault();
@@ -8,3 +7,51 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
   });
 });
+
+const videos = document.querySelector("video");
+const audios = document.querySelector("audio")
+
+let players = [];
+
+for (let i = 0; i < audios.clientHeight; i++) {
+  players.push(audios[i])
+}
+
+for (let i = 0; i < videos.clientHeight; i++) {
+  players.push(videos[i])
+}
+
+for (let i = 0; i < players.length; i++) {
+  players[i].removeAttribute('controls')
+}
+
+function playerController(player, playPauseButton, stopButton, forwardButton, timeLabel) {
+  this.player = player;
+  this.playPauseButton = playPauseButton;
+  this.stopButton = stopButton;
+  this.forwardButton = forwardButton;
+  this.timeLabel = timeLabel;
+  this.interval;
+  this.playPauseButton.onClick = function() {
+    if (player.paused) {
+      player.play();
+    } else {
+      player.pause();
+      playPauseButton.textContent = "Play"
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
